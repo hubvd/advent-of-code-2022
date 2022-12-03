510cdf644ba57c2c3e6328c6f0190b2ddb01c3c5
Hubert Van De Walle <hubv@protonmail.com>
2022-12-03 14:01:59 +0100

    Improve Day 3

--- aoc/src/Day03.kt
+++ aoc/src/Day03.kt
@@ -1,12 +1,12 @@
 fun main() {
     val letters = listOf('#') + ('a'..'z') + ('A'..'Z')
+    fun Iterable<String>.priority() = map { it.toHashSet() }.reduce<Set<Char>, _> { acc, s -> acc.intersect(s) }
+        .let { letters.indexOf(it.first()) }
     Input(3).lineSequence()
-        .map { it.substring(0..<it.length / 2) to it.substring(it.length / 2..<it.length) }
-        .map { (left, right) -> left.toHashSet().intersect(right.toHashSet()) }
-        .sumOf { letters.indexOf(it.first()) }
+        .map { listOf(it.substring(0..<it.length / 2), it.substring(it.length / 2..<it.length)) }
+        .sumOf { it.priority() }
         .also { println(it) }
     Input(3).lineSequence().chunked(3)
-        .map { lines -> lines.map { it.toHashSet() }.reduce<Set<Char>, _> { acc, s -> acc.intersect(s) } }
-        .sumOf { letters.indexOf(it.first()) }
+        .sumOf { it.priority() }
         .also { println(it) }
 }
