b1ca14c1d549ebc32b7136d45d2f73e7b766f605
Hubert Van De Walle <hubv@protonmail.com>
2022-12-04 18:08:31 +0100

    Clean

--- aoc/src/Day04.kt
+++ aoc/src/Day04.kt
@@ -1,8 +1,7 @@
 val day4solver = solver(4) {
-    val sectionRe = """^(\d+)-(\d+),(\d+)-(\d+)$""".toRegex()
-    lines().map { sectionRe.find(it)!!.destructured.toList().map { it.toInt() } }
-        .map { (a, b, c, d) -> a..b to c..d }
-        .run {
+    Regex("\\d+").findAll(text()).map { it.value.toInt() }
+        .chunked(2).map { it[0]..it[1] }
+        .chunked(2).run {
             count { (a, b) -> a.all { it in b } || b.all { it in a } }.yield()
             count { (a, b) -> a.any { it in b } || b.any { it in a } }.yield()
         }
