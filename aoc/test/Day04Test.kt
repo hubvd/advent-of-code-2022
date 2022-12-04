2f7d7d2812669fcab237ad62856b6fc311fcfd5f
Hubert Van De Walle <hubv@protonmail.com>
2022-12-04 14:46:50 +0100

    Add Day 4

---
+++ aoc/test/Day04Test.kt
@@ -0,0 +1,15 @@
+class Day04Test : SolverSpec(4, {
+    example = """
+    2-4,6-8
+    2-3,4-5
+    5-7,7-9
+    2-8,3-7
+    6-6,4-6
+    2-6,4-8
+    """.trimIndent()
+
+    part1Example = 2
+    part1 = 644
+    part2Example = 4
+    part2 = 926
+})
